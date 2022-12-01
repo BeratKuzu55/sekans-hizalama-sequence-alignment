c28fff2c6b1aabb716b60733c57230170e2c9304
Berat Kuzu <75533278+BeratKuzu55@users.noreply.github.com>
2022-12-02 01:12:19 +0300

    Add files via upload

---
+++ Biyolojiodev2/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Biyolojiodev2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            bool kontrol = false;
+            for (int i = 0; i < textBox1.Text.Length; i++)
+            {
+                if(textBox1.Text[i]<48 || textBox1.Text[i]>57)
+                {
+                    kontrol = true;
+                }
+            }
+
+
+
+            int k_val = 0;
+            string k = textBox1.Text;
+            
+            if (k == "")
+            {
+                string message = "Lutfen K degerini giriniz ( ornek K: 1)";
+                string title = "uyari";
+                MessageBox.Show(message, title);
+            }
+            else if(kontrol)
+            {
+                string message = "Lutfen K degerini dogru giriniz ( ornek K: 1)";
+                string title = "uyari";
+                MessageBox.Show(message, title);
+            }
+            else
+            {
+                k_val = int.Parse(k);
+                if (k_val > 0)
+                {
+                    Form2 form2_ornek1 = new Form2(richTextBox1.Text, richTextBox2.Text, k_val);
+                    form2_ornek1.Show();
+                }
+                else
+                {
+                    string message2 = "K degeri 0 dan buyuk olmali";
+                    string title = "uyari";
+                    MessageBox.Show(message2, title);
+                }
+
+
+            }
+
+
+
+        }
+    }
+}
---
+++ Biyolojiodev2/Form2.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Biyolojiodev2
+{
+    public partial class Form2 : Form
+    {
+        int query_lenght, target_lenght, k_val;
+        string query, target;
+
+        public Form2(string query, string target, int k_val)
+        {
+            InitializeComponent();
+            this.query = query; this.target = target; this.k_val = k_val;
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            query_lenght = query.Length; target_lenght = target.Length;
+            dataGridView1.ColumnCount = query_lenght + 2;
+            dataGridView1.RowCount = target_lenght + 1;
+
+            //TARGET IN TABLODA YAZILMASI
+            for (int i = 0; i < query_lenght; i++)
+            {
+                dataGridView1.Columns[1].Name = "j";
+                dataGridView1.Columns[i + 2].Name = (i + 1).ToString();
+                for (int j = 0; j < target_lenght; j++)
+                {
+                    dataGridView1.Rows[j + 1].Cells[1].Value = target[j];
+                }
+            }
+
+            //QUERY NİN TABLODA YAZILMASI
+            for (int i = 0; i < target_lenght; i++)
+            {
+                dataGridView1.Rows[0].Cells[0].Value = "i";
+                dataGridView1.Rows[i + 1].Cells[0].Value = i + 1;
+                for (int j = 0; j < query_lenght; j++)
+                {
+                    dataGridView1.Rows[0].Cells[j + 2].Value = query[j];
+
+                }
+
+            }
+
+
+
+            for (int i = 0; i < target_lenght; i++)
+            {
+                for (int j = 0; j < query_lenght; j++)
+                {
+                    if (target[i] == query[j])
+                    {
+
+                        dataGridView1.Rows[i + 1].Cells[j + 2].Value = "*";
+
+                    }
+                }
+            }
+
+
+
+
+
+
+            int kontrol = 0;
+            int kontrol2 = 0;
+            for (int i = 0; i < target_lenght; i++)
+            {
+
+                for (int j = 0; j < query_lenght; j++)
+                {
+
+                    // suan icinde bulundugumuz 2 for dongusuyle bütün hücrelere tek tek bakıyoruz 
+
+                    int r = 0;
+                    if (target[i] == query[j])
+                    {
+
+                        kontrol = 1;
+                        int i1 = i, i2 = i; int j1 = j;
+                        int kontrol_target = target_lenght - i; int kontrol_query = query_lenght - j;
+                        int min = Math.Min(kontrol_query, kontrol_target);
+
+                        //target ve query arasında esitlik buldugumuzda "aynı sırada" baska esitlikler de var mı diye 
+                        //asagıdaki for dongusunde bakıyoruz ve bu for dongusu min kadar donuyor 
+
+                        //Random rnd = new Random();
+                        Color random_color;
+                        for (int q = 0; q < min; q++)
+                        {
+
+    
+                            if (i2 == target_lenght - 1 || j1 == query_lenght - 1)
+                            {
+                                break;
+                            }
+
+
+                            if (target[i2 + 1] == query[j1 + 1])
+                            {
+                                kontrol += 1;
+                              
+                            }
+                            else
+                            {
+                                break;
+                            }
+                            i2 += 1; j1 += 1;
+
+
+                        }
+
+
+
+                        if (kontrol >= k_val)
+                            kontrol2 += 1;
+                        Color[] color_array = new Color[kontrol2];
+                        Random rnd;
+
+                        for (int s = 0; s < kontrol2; s++)
+                        {
+                            rnd = new Random();
+                            random_color = Color.FromArgb(rnd.Next(256), rnd.Next(256), rnd.Next(256));
+                            color_array[s] = random_color;
+
+                        }
+
+                        int kontrol3 = 0;
+                        if (kontrol >= k_val)
+                        {
+                            kontrol3 += 1;
+
+                            dataGridView1.Rows[i + 1].Cells[j + 2].Style.BackColor = Color.Green;//color_array[r++];
+                            dataGridView1.Rows[i + 1].Cells[j + 2].Value += "1";
+                            
+
+                        }
+                        else
+                        {
+
+                            if(dataGridView1.Rows[i].Cells[j + 1].Style.BackColor == Color.Green)
+                            {
+                                dataGridView1.Rows[i + 1].Cells[j + 2].Style.BackColor = Color.Green;//color_array[r++];
+                            }
+                        }
+
+                      
+
+
+                    }
+
+                    
+
+                }
+            }
+
+            
+        }
+    }
+}
